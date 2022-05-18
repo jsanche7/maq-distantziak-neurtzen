1152f8f0436b85f6115b534c23361acf081f1453
jsanche7 <80990132+jsanche7@users.noreply.github.com>
2022-05-18 13:34:36 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,28 @@
 basic.forever(function () {
-    basic.showNumber(DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2))
+    if (DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2) > 60) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 250)
+    } else if (DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2) > 40) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 150)
+    } else if (DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2) > 30) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 100)
+    } else if (DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2) > 20) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 50)
+    } else if (DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2) > 10) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 25)
+    } else if (DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2) > 7) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 5)
+    } else {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 0)
+    }
 })
 basic.forever(function () {
-	
+    if (true) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    }
 })
